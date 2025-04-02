aad62de1957f38ac1ed14d4e681a41da55861fd0
ajaymr2003 <ajaymr20033002@gmail.com>
2025-04-02 15:26:56 +0530

    COMPLETED

--- rbinary.java
+++ rbinary.java
@@ -25,16 +25,16 @@ public class rbinary {
         }
 
         // Sort both arrays together
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+        for (int i = 0; i < n - 1; i++) 
+            for (int j = i+1; j < n; j++) 
+                if (arr[i] > arr[j]) {
                     // Swap elements in both arrays
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
-            }
-        }
+            
+  
 
         // Print the sorted array
         System.out.println("Sorted array");
